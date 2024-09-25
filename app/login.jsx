dcae636c6ccc9339a2964ab0ad9f089831b13bcf
poorvatuteja74 <168651056+poorvatuteja74@users.noreply.github.com>
2024-09-24 20:45:06 -0400

    edit the margin of login screen

--- app/login.jsx
+++ app/login.jsx
@@ -78,7 +78,6 @@ export default Login;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // paddingTop: hp(2),
     gap:45,
     paddingHorizontal: wp(5),
   },
@@ -113,7 +112,7 @@ const styles = StyleSheet.create({
     textAlign: 'right',
     fontWeight: theme.fonts.semibold,
     color: theme.colors.text,
-    // marginBottom: hp(3),
+    marginBottom: hp(3),
   },
   footer: {
     flexDirection: 'row',
--- app/signUp.jsx
+++ app/signUp.jsx
@@ -149,4 +149,5 @@ const styles = StyleSheet.create({
   inputText: {
     color: theme.colors.text,
   },
+
 });
